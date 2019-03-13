48ca497a527eac2f2c9f92d596b336e2e4f9fbaf
Fares Eidi <fares.eidi@gmail.com>
2019-03-13 11:23:17 +0100

    Display Cards

--- src/app/cards/cards.page.ts
+++ src/app/cards/cards.page.ts
@@ -1,15 +1,15 @@
+import { CardService } from './../services/card.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
-  templateUrl: './cards.page.html',
-
+  templateUrl: './cards.page.html'
 })
 export class CardsPage implements OnInit {
-
-  constructor() { }
+  cards: string[];
+  constructor(private cardService: CardService) {}
 
   ngOnInit() {
+    this.cards = this.cardService.fakeCardList;
   }
-
 }
